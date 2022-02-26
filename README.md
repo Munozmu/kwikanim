974569b05a6b32d32d4b3750d70ff521ca52c882
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2022-02-26 13:08:17 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kwikanim
 Quick, simple smooth animation javascript base
+
+![Alt text](./kwikanim-logo.png "a title")
---
+++ animations-script.js
@@ -0,0 +1,45 @@
+// Thanks to this script, I can animate on scroll every element
+// on the page, simply by adding a 'animate-on-scoll' attribute
+// to the element.
+
+// V 1.0
+// Gabin Munoz
+
+
+function animationInit() {
+    var elements;
+    var windowHeight;
+
+    // Pourcentage de déclenchement de l'animation
+    var triggeringPercentage = 30;
+
+    function init() {
+        elements = document.querySelectorAll('.hidden');
+        windowHeight = window.innerHeight;
+    }
+
+    function checkPosition() {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var positionFromTop = elements[i].getBoundingClientRect().top;
+
+
+            // Ici on calcule 10% de la hauteur de l'écran pour éviter que l'animation ne se lance pile au moment où elle 
+            // passe la fenêtre.
+            if (positionFromTop - windowHeight <= (-(triggeringPercentage * windowHeight) / 100)) {
+                animateElement(element, element.getAttribute('animation-on-scroll'));
+            }
+        }
+    }
+
+    function animateElement(theElement, animationName) {
+        theElement.classList.add(animationName);
+        theElement.classList.remove('hidden');
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+
+    init();
+    checkPosition();
+}
